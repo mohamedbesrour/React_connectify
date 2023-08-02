1b33b9f89117d14d5961e1db38d687d5f22e0bc7
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-08-02 12:58:12 +0200

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,44 @@
 import ReactLogo from '../logo.svg'
 import '../styles/header.css'
+import '../styles/navBarFooter.css'
+import video from "../Image/videoConnectify.mp4"
+// import navBar from ""
 export default function Header(){
     return(
         <header className='headerContainer'>
             <h1>Les composants React</h1>
             <img src={ReactLogo} alt='logo React'></img>
+            
+            <div className="nav">
+  <input type="checkbox" id="nav-check" />
+  <div className="nav-header">
+    <div className="nav-title">
+      <a href="index.html">
+        <img src="image/games.png" />
+      </a>
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label htmlFor="nav-check">
+      <span />
+      <span />
+      <span />
+    </label>
+  </div>
+  <div className="nav-links">
+    <a href="./html/jeu.html">Accueil</a>
+    <a href="./html/regle.html">Connexion</a>
+    <a href="./html/support.html">Inscription</a>
+  </div>
+</div>
+           
+            <video src={video} id="background-video" autoPlay loop muted></video>
+
+<div class="content">
+<h1>
+Connectify
+</h1>
+</div>
         </header> 
     )
 }
