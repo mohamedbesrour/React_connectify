a9638f74e25e401cbe4adec9b3df40179fb7d307
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2023-08-04 09:44:24 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,53 @@
-import ReactLogo from '../logo.svg'
-import '../styles/header.css'
-import '../styles/navBarFooter.css'
-import video from "../Image/videoConnectify.mp4"
+// import ReactLogo from "../logo.svg";
+import "../styles/header.css";
+import "../styles/navBarFooter.css";
+import video from "../Image/videoConnectify.mp4";
 // import navBar from ""
-export default function Header(){
-    return(
-        <header className='headerContainer'>
-            <h1>Les composants React</h1>
-            <img src={ReactLogo} alt='logo React'></img>
-            
-            <div className="nav">
-  <input type="checkbox" id="nav-check" />
-  <div className="nav-header">
-    <div className="nav-title">
-      <a href="index.html">
-        <img src="image/games.png" />
-      </a>
-    </div>
-  </div>
-  <div className="nav-btn">
-    <label htmlFor="nav-check">
-      <span />
-      <span />
-      <span />
-    </label>
-  </div>
-  <div className="nav-links">
-    <a href="./html/jeu.html">Accueil</a>
-    <a href="./html/regle.html">Connexion</a>
-    <a href="./html/support.html">Inscription</a>
-  </div>
-</div>
-           
-            <video src={video} id="background-video" autoPlay loop muted></video>
+export default function Header() {
+  return (
+    <header className="headerContainer">
+      <div className="Accueil">
+        
+        {/* <img src={ReactLogo} alt='logo React'></img> */}
 
-<div class="content">
-<h1>
-Connectify
-</h1>
-</div>
-        </header> 
-    )
+        <div className="nav">
+          <input type="checkbox" id="nav-check" />
+          <div className="nav-header">
+            <div className="nav-title">
+              <a href="index.html">
+                <h2 className="logo">Connectify</h2>
+              </a>
+            </div>
+          </div>
+          <div className="nav-btn">
+            <label htmlFor="nav-check">
+              <span />
+              <span />
+              <span />
+            </label>
+          </div>
+          <div className="nav-links">
+            <a href="./html/jeu.html">Accueil</a>
+            <a href="./html/regle.html">Connexion</a>
+            <a href="./html/support.html">Inscription</a>
+          </div>
+        </div>
+
+        <video src={video} id="background-video" autoPlay loop muted></video>
+
+        <div class="content">
+          <h1>Connectify</h1>
+        </div>
+        
+        {/* <div className="blocAcc1">
+        <div>
+          <h2 className="bienvenue">Bienvenue</h2></div>
+          <p>"Harmonisez vos passions, partagez vos émotions avec Connectify !"</p>
+        </div>
+        <div className="blocAcc2"></div>
+        <div className="blocAcc3"></div>
+        <div className="blocAcc4"></div> */}
+      </div>
+    </header>
+  );
 }
