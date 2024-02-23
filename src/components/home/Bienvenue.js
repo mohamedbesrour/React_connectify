38c5ab19d85c18104eb7a3af89f219e8938206d6
mohamedbesrour <118281616+mohamedbesrour@users.noreply.github.com>
2024-02-23 23:50:46 +0100

    correctif composant "Bienvenue"

--- src/components/home/Bienvenue.js
+++ src/components/home/Bienvenue.js
@@ -1,55 +1,34 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const traductions = [
-    "Bienvenue",
-    "Welcome",
-    "Bienvenido",
-    "Willkommen",
-    "ben vini",
-    "Byenvini",
-    "Akksil ak diam",
-    "дякую",
-    "স্বাগত",
-    "欢迎光临",
-    'مرحباً',
-    "ברוך הבא",
-    "いらっしゃいませ",
-    "Anṣuf yis-wen",
-    "ⴰⵏⵚⵓⴼ ⵢⵉⵙ ⵡⴻⵏ",
+  "Bienvenue","Welcome","Bienvenido","Willkommen",
+  "ben vini","Byenvini","Akksil ak diam","дякую",
+  "স্বাগত","欢迎光临","مرحباً","ברוך הבא",
+  "いらっしゃいませ","Anṣuf yis-wen","ⴰⵏⵚⵓⴼ ⵢⵉⵙ ⵡⴻⵏ",
 ];
 
+export default function Bienvenue() {
+  const [index, setindex] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setindex((prevIndex) =>
+        prevIndex === traductions.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
+  return (
+    <div className="containerBienvenue">
+      <div className="titreBienvenue">
+        <h2>{traductions[index]}</h2>
+        <p className="texteBienvenue">
+          "Harmonisez vos passions, partagez vos émotions
+          avec Connectify!"
+        </p>
+      </div>
+    </div>
+  );
+}
 
-    function Bienvenue() {
-        const [index, setindex] = useState(0)
-
-        useEffect(() => {
-            const interval = setInterval(() => {
-                setindex((prevIndex) =>
-                    prevIndex === traductions.length - 1 ? 0 : prevIndex + 1);
-            }, 1000);
-            return () => clearInterval(interval);
-        }, []);
-        return (
-
-            // <div className="Bienvenue">
-            //     <h1 >{traductions[index]}</h1>
-            //     <p>"Harmonisez vos passions,
-            //         partagez vos émotions avec
-            //         Connectify !"</p>
-            // </div>
-
-
-
-            <div className="containerBienvenue">
-                <div className="titreBienvenue">
-                    <h2>{traductions[index]}</h2>
-                        <p className="texteBienvenue">
-                        "Harmonisez vos passions, partagez vos émotions avec Connectify!"
-                        </p>
-                </div>
-            </div>
-        )
-    }
-    export default Bienvenue
